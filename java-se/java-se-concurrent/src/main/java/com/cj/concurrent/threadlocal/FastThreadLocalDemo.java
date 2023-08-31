69a9c83bc13617763470f8748c1c6019d3eac878
cj <cj@git.com>
2023-08-31 17:24:12 +0800

    threadlocal

---
+++ java-se/java-se-concurrent/src/main/java/com/cj/concurrent/threadlocal/FastThreadLocalDemo.java
@@ -0,0 +1,9 @@
+package com.cj.concurrent.threadlocal;
+
+import io.netty.util.concurrent.FastThreadLocal;
+
+public class FastThreadLocalDemo {
+    public static void main(String[] args) {
+        new FastThreadLocal<>();
+    }
+}
