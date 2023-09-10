41467cd45d0241755311f05451fe9534ee188746
cj <cj@git.com>
2023-09-10 19:34:24 +0800

    spring boot

---
+++ java-ee/java-dev-springboot/src/main/java/com/example/javadevspringboot/JavaDevSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.javadevspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevSpringbootApplication.class, args);
+    }
+
+}
