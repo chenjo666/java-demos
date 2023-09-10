7c1a003494caf8e55a85d9f58da689167de785f8
cj <cj@git.com>
2023-09-10 19:57:54 +0800

    string

---
+++ java-base/src/main/java/com/cj/string/StringBufferDemo.java
@@ -0,0 +1,7 @@
+package com.cj.string;
+
+public class StringBufferDemo {
+    public static void main(String[] args) {
+        StringBuffer sb  = new StringBuffer();
+    }
+}
---
+++ java-base/src/main/java/com/cj/string/StringBuilderDemo.java
@@ -0,0 +1,8 @@
+package com.cj.string;
+
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+    }
+}
