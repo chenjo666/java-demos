189614d3cb4d254be7928dd6045f9b48a1bfdd8b
cj <cj@git.com>
2023-10-05 15:58:46 +0800

    注释autowired

--- java-dev/sms-spring-boot-starter/src/main/java/com/cj/service/SmsTemplate.java
+++ java-dev/sms-spring-boot-starter/src/main/java/com/cj/service/SmsTemplate.java
@@ -12,12 +12,12 @@ public class SmsTemplate {
     @Autowired
     private ApplicationContext context;
 
-    public String send(String fromPhone, String toPhone, String content) {
-        if (!smsProperties.isEnable()) {
-            throw new RuntimeException("操作失败！请先开启邮件服务");
-        }
-        String type = smsProperties.getType();
-        SmsService smsService = context.getBean(type, SmsService.class);
-        return smsService.send(fromPhone, toPhone, content);
-    }
+//    public String send(String fromPhone, String toPhone, String content) {
+//        if (!smsProperties.isEnable()) {
+//            throw new RuntimeException("操作失败！请先开启邮件服务");
+//        }
+//        String type = smsProperties.getType();
+//        SmsService smsService = context.getBean(type, SmsService.class);
+//        return smsService.send(fromPhone, toPhone, content);
+//    }
 }
