034bfd109765f176e4e3fef630a2c9ca67b0881d
cj <cj@git.com>
2024-02-05 09:17:10 +0800

    feat: 新增feign拦截器

--- java-dev/java-dev-springcloud/feign-client/src/main/java/com/cj/feign/config/FeignConfiguration.java
+++ java-dev/java-dev-springcloud/feign-client/src/main/java/com/cj/feign/config/FeignConfiguration.java
@@ -1,11 +1,23 @@
 package com.cj.feign.config;
 
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 public class FeignConfiguration {
     @Bean
     public Logger.Level loglevel() {
         return Logger.Level.BASIC;
+    }
+
+    @Bean
+    public RequestInterceptor userInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                requestTemplate.header("user", "123");
+            }
+        };
     }
 }
